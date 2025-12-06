5bc649f1e59ad5ccb5fb2776d64d058a77e1e79d
Lamission <200~72870124+Lamission877@users.noreply.github.com>
2025-12-06 21:57:48 +0200

    testing it now

--- Content.Shared/Paper/PoisonPenSystem.cs
+++ Content.Shared/Paper/PoisonPenSystem.cs
@@ -1,26 +1,37 @@
 namespace Content.Shared.System;
+namespace Content.Server.Chemistry.EntitySystems;
 
 public sealed class PoisonPenSystem : EntitySystem
 {
     [Dependency] private readonly SharedSolutionContainerSystem _solutionContainer = default!;
+    [Dependency] private readonly TagSystem _tag = default!;
 
     public override void Initialize()
-        {
-            base.Initialize();
+    {
+        base.Initialize();
 
-            SubscribeLocalEvent<PoisonPenComponent, AfterInteractEvent>(AfterInteract);
+        SubscribeLocalEvent<PoisonPenComponent, AfterInteractEvent>(AfterInteract);
+        SubscribeLocalEvent<PoisonPenComponent, ComponentInit>(OnCompInit);
+    }
 
-        }
+    private void OnCompInit(Entity<PoisonPenComponent> ent, ref ComponentInit Args)
+    {
+        EnsureComp<SolutionContainerComponent>(ent.Owner);
+        EnsureComp<InjectorComponent>(ent.Owner);
+    }
+
 
     private void AfterInteract(Entity<PoisonPenComponent> ent, AfterInteractEvent args)
     {
-        if(HasComp<PaperComponent>(args.Target))
+        if(!_tag.HasTag(args.target, "Paper")) //Checks if it's a paper
         {
             return;
         }
-        if(TryComp<SolutionInjectOnPickupComponent(args.Target, out var poison))
+
+
+        if(TryComp<SolutionInjectOnPickupComponent>(args.Target, out var poison))
         {
-            _solutionContainer.TryGetSolution(ent.Owner, ent.Comp.Solution, out var solution);
+            TryUseInjector(ent, args.target, args.user);
         }
     }
 }
